20020280891ac62110db2d1d4e5ade4f1f2fd7f3
archiedms <archie.dumas2006@gmail.com>
2025-11-28 16:36:37 +0000

    Add programme field and update Lecturer constructor

--- src/model/Lecturer.java
+++ src/model/Lecturer.java
@@ -4,11 +4,21 @@ public class Lecturer {
     private String id;
     private String name;
 
-    public Lecturer(String id, String name) {
+    // 1. ADD THIS FIELD
+    private Programme programme;
+
+    // 2. MODIFY CONSTRUCTOR TO ACCEPT PROGRAMME
+    public Lecturer(String id, String name, Programme programme) {
         this.id = id;
         this.name = name;
+        this.programme = programme;
     }
 
     public String getId() { return id; }
     public String getName() { return name; }
+
+    // 3. ADD THIS GETTER METHOD
+    public Programme getProgramme() {
+        return programme;
+    }
 }
